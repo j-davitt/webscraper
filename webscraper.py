f166100637fc984312bcc074c9f85a801f805cf0
Joseph Davitt <joseph.j.davitt@gmail.com>
2023-10-12 17:50:26 -0700

    ingredients scraped

--- webscraper.py
+++ webscraper.py
@@ -1,8 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = 'https://www.imdb.com/chart/top/?ref_=nv_mv_250'
+url = 'https://www.allrecipes.com/recipe/268494/basic-air-fryer-hot-dogs/'
 response = requests.get(url, timeout=5)
 content = BeautifulSoup(response.text, 'html.parser')
 
-print(content)
+# recipe = content.findAll('li', attrs={"class": "mntl-structured-ingredients__list-item"}).text
+
+for recipe in content.findAll('li', attrs={"class": "mntl-structured-ingredients__list-item"}):print (recipe.text.encode('utf-8'))
+
+# print(recipe)
